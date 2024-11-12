28f231cfb1e918d16e7067a81894bb9cf0c870d4
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-11-12 15:37:28 +0100

    tkghrugh

--- yunus-store/medusa-config.ts
+++ yunus-store/medusa-config.ts
@@ -4,7 +4,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: process.env.DATABASE_URL || "postgresql://neondb_owner:2BxVCzWPF6jw@ep-delicate-wind-a2jx1y9d.eu-central-1.aws.neon.tech/neondb?sslmode=require",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
